5eaa4e3cb3e1eebf5be516ca182686cfd4432cdd
devilstan <devilstan@gmail.com>
2015-12-21 22:42:39 +0800

    bat

--- bin/Debug/fire.bat
+++ bin/Debug/fire.bat
@@ -1,2 +1,2 @@
 echo off
-start "C:\Users\devilstan\Documents\Visual Studio 2015\Projects\Gantt Chart Tester\bin\Debug\" gangan.exe C:\Users\devilstan\Downloads\BT_1
+start "C:\Users\devilstan\Documents\Visual Studio 2015\Projects\Gantt Chart Tester\bin\Debug\" gangan_dev.exe C:\Users\devilstan\Downloads\BT_1
